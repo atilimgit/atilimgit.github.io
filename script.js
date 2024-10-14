1e52eeff3ad689eea220900ed190ec1c84ddbd4b
AGIT <erarslan.umutemre@student.atilim.edu.tr>
2024-10-14 21:14:15 +0300

    fixess

--- script.js
+++ script.js
@@ -36,13 +36,7 @@ function renderBusinessList(page) {
 
 function toggleBusinessDetails(businessId) {
     const detailDiv = document.getElementById(`detail-${businessId}`);
-    const isVisible = detailDiv.classList.contains('show');
-
-    if (isVisible) {
-        detailDiv.classList.remove('show');
-    } else {
-        detailDiv.classList.add('show');
-    }
+    detailDiv.style.display = detailDiv.style.display === "none" || detailDiv.style.display === "" ? "block" : "none";
 }
 
 function filterBusinesses(businesses) {
